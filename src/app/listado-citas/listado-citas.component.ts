65c22c40e114cd21d0841e1bab03d8e201d04368
unknown <bryanpastrana42@gmail.com>
2022-05-24 00:26:50 +0200

    v1.1

--- src/app/listado-citas/listado-citas.component.ts
+++ src/app/listado-citas/listado-citas.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { async } from '@angular/core/testing';
+import { BookingService } from '../services/booking/booking.service';
+import { CitasUsuariosService } from '../services/citas-usuarios/citas-usuarios.service';
 
 @Component({
   selector: 'app-listado-citas',
@@ -7,9 +10,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListadoCitasComponent implements OnInit {
 
-  constructor() { }
+  allBooking: any
+  allUser: any
+  infoDataBooking = new Array();
+  constructor(private users: CitasUsuariosService, private booking: BookingService) { }
 
   ngOnInit(): void {
+    this.getAllBooking();
+    this.getAllUsers();
+  }
+
+  getAllBooking(){
+    this.booking.findAllBooking().subscribe(async booking =>{
+      this.allBooking = booking;
+    })
+  }
+
+  getAllUsers(){
+    this.users.findAllUser().subscribe(async users => {
+      this.allUser = users;
+    })
   }
 
 }
